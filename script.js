97f59d87a502a536d68d41bf4856915e33a470ac
Keyluce <86408122+Keyluce@users.noreply.github.com>
2024-09-19 15:30:39 +0530

    Remove Items and Clear All)

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
   e.preventDefault();
@@ -21,6 +23,7 @@ function addItem(e) {
 
   itemList.appendChild(li);
 
+  checkUI();
   itemInput.value = '';
 }
 function createButton(classes) {
@@ -35,6 +38,38 @@ function createIcon(classes) {
   icon.className = classes;
   return icon;
 }
+function removeItem(e) {
+  // if (e.target.tagName == 'I') {
+  //   e.target.parentElement.parentElement.remove();
+  // }
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+function clearItems(e) {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+    // itemList.firstChild.remove();
+  }
+  checkUI();
+}
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
 // Event Listeners
 
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+
+checkUI();
